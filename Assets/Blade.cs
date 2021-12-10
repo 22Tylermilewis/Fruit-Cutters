7a28c097203c58b11a56511b2d9d3dba22ae5ea0
22Tylermilewis <89477398+22Tylermilewis@users.noreply.github.com>
2021-12-10 13:44:25 -0500

    Upload the dang code next time

---
+++ Assets/Blade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    bool isCutting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void UpdateCut()
+    {
+
+    }
+    void StartCutting ()
+    {
+        isCutting = true;
+    }
+
+    void StopCutting ()
+    {
+        isCutting = false;
+    }
+}
